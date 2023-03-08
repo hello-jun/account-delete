12cb24d3aef8ce29ac0638154b01b98ccbdf63ae
jun24.zhang <jun24.zhang@tcl.com>
2023-03-08 14:53:32 +0800

    feat: init

---
+++ src/components/my-component/my-component.tsx
@@ -0,0 +1,43 @@
+/*
+ * @Author: zhangjun
+ * @Date: 2023-03-08 11:46:16
+ * @LastEditors: zhangjun
+ * @LastEditTime: 2023-03-08 11:55:09
+ * @Description: 
+ * @FilePath: /src/components/my-component/my-component.tsx
+ */
+import { Component, Prop, h } from '@stencil/core';
+import { format } from '../../utils/utils';
+
+@Component({
+  tag: 'my-component',
+  styleUrl: 'my-component.css',
+  shadow: true,
+})
+export class MyComponent {
+  /**
+   * The first name
+   */
+  @Prop()
+  first!: string
+
+  /**
+   * The middle name
+   */
+  @Prop()
+  middle!: string
+
+  /**
+   * The last name
+   */
+  @Prop()
+  last!: string
+
+  private getText(): string {
+    return format(this.first, this.middle, this.last);
+  }
+
+  render() {
+    return <div>Hello, World! I'm {this.getText()}</div>;
+  }
+}
